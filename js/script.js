4cf62cf2bc1e978af7f3530aaba27c39a2dcafc5
KatalinaNadasi <katalina.nadasi@gmail.com>
2017-06-01 16:56:07 +0200

    rajout résultats de recherche en javascript

--- js/script.js
+++ js/script.js
@@ -58,22 +58,88 @@ $(function(){
       width: '100%'
     }, 400);
     $(icon).animate({
-      right: '5px'
+      right: '-2px'
     }, 400);
   });
 
-//Blur event
-$(searchBar).blur(function(){
-  if(searchBar.val() == ''){
-    $(searchBar).animate({
-      width: '45%'
-    }, 400);
-    $(icon).animate({
-      right: '274px'
-    }, 400, function(){});
+  //Blur event
+  $(searchBar).blur(function(){
+    if(searchBar.val() == ''){
+      $(searchBar).animate({
+        width: '45%'
+      }, 400);
+      $(icon).animate({
+        right: '274px'
+      }, 400, function(){});
+    }
+  });
+  $('#search-form').submit(function(e){
+    e.preventDefault();
+  })
+})
+
+function search(){
+  //clear results
+  $('#results').html('');
+  $('#buttons').html('');
+
+  //get form input
+  q = $('#query').val();
+
+  //run GET Request on API
+  $.get(
+    "https://www.googleapis.com/youtube/v3/search",{
+      part: 'snippet, id',
+      q: q,
+      type: 'video',
+      key: 'AIzaSyAooqgNg4nX1yemhMafbx6voR13GRr78mo'},
+      function(data){
+        var nextPageToken = data.nextPageToken;
+        var prevPageToken = data.prevPageToken;
+        //Log Data
+        console.log(data);
+
+        $.each(data.items, function(i, item){
+          //Get Output
+          var output = getOutput(item);
+          //Display Results
+          $('#results').append(output);
+        });
+        //Next page and Previous page Buttons
+        var buttons = getButtons(prevPageToken, nextPageToken);
+        //Display Buttons
+        $('#buttons').append(buttons);
+      }
+    );
+}
+
+//Build Output
+function getOutput(item){
+  var videoId = item.id.videoId;
+  var title = item.snippet.title;
+  var description = item.snippet.description;
+  var thumb = item.snippet.thumbnails.high.url;
+  var channelTitle = item.snippet.channelTitle;
+  var videoDate = item.snippet.publishedAt;
+
+  //Build Output String
+  var output = '<li>' + '<div class="list-left">' + '<img src="'+thumb+'">' + '</div>' + '<div class="list-right">' + '<h3>' +title+ '</h3>' + '<small>By <span class="cTitle">'+channelTitle+'</span>on '+videoDate+'</small>' + '<p>'+description+'</p>' + '</div>' + '</li>' + '<div class="clearfix"></div>' + '';
+
+  return output;
+};
+
+//Build the buttons
+function getButtons(prevPageToken, nextPageToken){
+  if(!prevPageToken){
+    var btnOutput = '<div class="button-container">' + '<button id="next-button" class="paging-button" data-token="'+nextPageToken+'" data-query="'+q+'"' + 'onclick="nextPage();">Next Page</button></div>';
+  } else {
+    var btnOutput = '<div class="button-container">' +
+    '<button id="prev-button" class="paging-button" data-token="'+prevPageToken+'" data-query="'+q+'">' +
+    'onclick="prevPage();">Previous Page</button>' +
+    '<button id="next-button" class="paging-button" data-token="'+nextPageToken+'" data-query="'+q+'">' +
+    'onclick="nextPage();">Next Page</button></div>';
   }
 
-});
+  return btnOutput;
 
-
-})
+}
